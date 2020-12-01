6524fbac7073779d511e8330a5fe9701f2e7eef7
MakarandPundlik <mmp2308@gmail.com>
2020-12-01 14:23:40 +0530

    some tricks

---
+++ Tips_Tricks/Trick02.java
@@ -0,0 +1,14 @@
+
+class Trick02
+{
+	
+	public static void main(String[] args)
+	{
+		//underscore in numeric literal
+		int x = 123_45;
+		System.out.println(x);
+	}
+}
+/*In later versions of java (>jdk 1.7)
+It is possible to assign numeric literals with unerscore
+*/
