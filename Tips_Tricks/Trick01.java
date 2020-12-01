07edba64e3c1766a51c68acbd1490f1441feb2fb
MakarandPundlik <mmp2308@gmail.com>
2020-12-01 14:18:37 +0530

    some tips

---
+++ Tips_Tricks/Trick01.java
@@ -0,0 +1,12 @@
+//tricks in java
+//comments can be excuted too
+class Trick01
+{
+	public static void main(String[] args)
+	{
+		// \u000d System.out.println("Welcome to Databyte Coaching Classes");
+	}
+} 
+/*This line of code will always execute because of Unicode 
+"\u000d" and java compiler parses this unicode character as newLine
+*/
